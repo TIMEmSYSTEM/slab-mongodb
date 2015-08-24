3652f788ab04c7da6c75e250afe886ccb59a246f
Dmytro Morozov <mdmsua@icloud.com>
2015-08-24 22:32:02 +0200

    Prepare solution for Travis

---
+++ sources/TIMEmSYSTEM.SemanticLogging.MongoDB/HumongousSink.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Practices.EnterpriseLibrary.SemanticLogging;
+
+namespace TIMEmSYSTEM.SemanticLogging.MongoDB
+{
+    public class HumongousSink : IObserver<EventEntry>, IDisposable
+    {
+        public void OnNext(EventEntry value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
